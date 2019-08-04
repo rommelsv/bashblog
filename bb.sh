c3996d39b67b074ab1e3ea838967416308e99215
David Satime Wallin <david@ganilla.se>
2019-08-04 13:35:01 +0200

    updated the google analytics script code

--- bb.sh
+++ bb.sh
@@ -208,18 +208,15 @@ google_analytics() {
     [[ -z $global_analytics && -z $global_analytics_file ]]  && return
 
     if [[ -z $global_analytics_file ]]; then
-        echo "<script type=\"text/javascript\">
 
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', '${global_analytics}']);
-        _gaq.push(['_trackPageview']);
+        echo "<!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src=\"https://www.googletagmanager.com/gtag/js?id=${global_analytics}\"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-        (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-
+        gtag('config', '${global_analytics}');
         </script>"
     else
         cat "$global_analytics_file"
